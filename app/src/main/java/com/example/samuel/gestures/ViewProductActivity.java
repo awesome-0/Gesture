2bf9a43779f4ab986ed30fca216e86d135f7c95a
samuel orji <awesomeorji@gmail.com>
2018-06-13 13:21:32 +0100

    added basic touch event listeners

--- app/src/main/java/com/example/samuel/gestures/ViewProductActivity.java
+++ app/src/main/java/com/example/samuel/gestures/ViewProductActivity.java
@@ -5,19 +5,24 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
 import java.util.ArrayList;
 
-public class ViewProductActivity extends AppCompatActivity {
+public class ViewProductActivity extends AppCompatActivity  implements View.OnTouchListener{
     Product mProduct;
     ViewPager pager;
     TabLayout tab;
+    private static final String TAG = "ViewProductActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_product);
         pager = findViewById(R.id.image_container);
+        pager.setOnTouchListener(this);
         tab = findViewById(R.id.tab);
         if(getIntent() != null && getIntent().hasExtra(getString(R.string.product))){
             mProduct = getIntent().getParcelableExtra(getString(R.string.product));
@@ -44,5 +49,28 @@ public class ViewProductActivity extends AppCompatActivity {
         fragmentAdapter adapter = new fragmentAdapter(getSupportFragmentManager(),fragments);
         pager.setAdapter(adapter);
         tab.setupWithViewPager(pager);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        int action = motionEvent.getAction();
+        switch(action){
+            case MotionEvent.ACTION_DOWN:
+                Log.d(TAG, "onTouch: ACTION_DOWN called");
+                return false;
+            case MotionEvent.ACTION_UP:
+                Log.d(TAG, "onTouch: ACTION_UP called");
+                return false;
+            case MotionEvent.ACTION_CANCEL:
+                Log.d(TAG, "onTouch: ACTION_CANCEL called");
+                return false;
+            case MotionEvent.ACTION_MOVE:
+                Log.d(TAG, "onTouch: ACTION_MOVE called");
+                return false;
+            case MotionEvent.ACTION_OUTSIDE:
+                Log.d(TAG, "onTouch: ACTION_OUTSIDE called");
+                return false;
+        }
+        return false;
     }
 }
